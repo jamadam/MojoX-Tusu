b9b4865ef7a3e98d0188af551f2062657dafe8a2
sugama <sugama@www.jamadam.com>
2011-05-18 00:24:58 +0900

    commented

--- lib/MojoX/Renderer/PSTemplate.pm
+++ lib/MojoX/Renderer/PSTemplate.pm
@@ -38,8 +38,8 @@ $VERSION = eval $VERSION;
         $engine->set_filename_trans_coderef(sub {
             _filename_trans($base_dir, @_);
         });
-        $engine->set_var(%{$c->stash->{vars}});
-        $engine->set_var(controller => $c);
+        $engine->set_var(%{$c->stash->{vars}}); ## not in use?
+        $engine->set_var(controller => $c); ## not in use?
         
         local $SIG{__DIE__} = undef;
         
