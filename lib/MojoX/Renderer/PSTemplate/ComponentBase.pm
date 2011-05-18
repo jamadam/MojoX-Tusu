ab9ddbff324632eff1c91bf44d83caafead90297
sugama <sugama@sv02.cutout.biz>
2011-05-18 21:37:09 +0900

    ComponentBase got some utility functions for template

--- lib/MojoX/Renderer/PSTemplate/ComponentBase.pm
+++ lib/MojoX/Renderer/PSTemplate/ComponentBase.pm
@@ -7,57 +7,79 @@ use base qw(Text::PSTemplate::PluginBase);
         
     }
     
+    sub param : TplExport {
+        
+		my ($self, $c, $name, $escape) = @_;
+		
+		my $val = $c->param($name);
+		if ($escape) {
+			$val = Text::PSTemplate::Plugin::HTML->escape($val);
+		}
+		return $val;
+    }
+    
+    sub post_param : TplExport {
+        
+		my ($self, $c, $name, $escape) = @_;
+		warn 'test';
+		my $val = $c->req->body_params->param($name);
+		if ($escape) {
+			$val = Text::PSTemplate::Plugin::HTML->escape($val);
+		}
+		return $val;
+    }
+    
     sub preceding_args {
         
         return $MojoX::Renderer::PSTemplate::controller;
     }
-    
-    sub get {
-        
-        my ($self, $c) = @_;
-        my $template = $c->stash('template') || '';
-        $c->render(
-            handler => 'pst',
-            format  => ($template =~ s{\.([^.]+)$}{}) ? $1 : 'html',
-            template => $template,
-        );
-    }
-    
-    sub post {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub head {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub put {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub delete {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub options {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub trace {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub patch {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub link {
-        die 'Must be implemented by sub class';
-    }
-    
-    sub unlink {
-        die 'Must be implemented by sub class';
-    }
+	
+	sub get {
+		
+		my ($self, $c) = @_;
+		my $template = $c->stash('template') || '';
+		$c->render(
+			handler => 'pst',
+			format	=> ($template =~ s{\.([^.]+)$}{}) ? $1 : 'html',
+			template => $template,
+		);
+	}
+	
+	sub post {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub head {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub put {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub delete {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub options {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub trace {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub patch {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub link {
+		die 'Must be implemented by sub class';
+	}
+	
+	sub unlink {
+		die 'Must be implemented by sub class';
+	}
 
 1;
 
@@ -68,42 +90,52 @@ __END__
 MojoX::Renderer::PSTemplate::ComponentBase - Base Class for WAF component
 
 =head1 SYNOPSIS
-    
-    package YourComponent;
-    use strict;
-    use warnings;
-    use base qw(MojoX::Renderer::PSTemplate::ComponentBase);
-    
-    sub get {
-        my ($self, $controller) = @_;
-        $controller->render(
-            handler => 'pst',
-            format  => ($template =~ s{\.([^.]+)$}{}) ? $1 : 'html',
-            template => $template,
-        );
-    }
-    sub post {
-        my ($self, $controller) = @_;
-        # ...
-    }
-    sub put {
-        my ($self, $controller) = @_;
-        # ...
-    }
-    # ...
-    sub some_func : TplExport {
-        my ($self, $controller, @your_args) = @_;
-        # ...
-        return '';
-    }
-    
-    <% some_func(@your_args) %>
+	
+	package YourComponent;
+	use strict;
+	use warnings;
+	use base qw(MojoX::Renderer::PSTemplate::ComponentBase);
+	
+	sub get {
+		my ($self, $controller) = @_;
+		$controller->render(
+			handler => 'pst',
+			format	=> ($template =~ s{\.([^.]+)$}{}) ? $1 : 'html',
+			template => $template,
+		);
+	}
+	sub post {
+		my ($self, $controller) = @_;
+		# ...
+	}
+	sub put {
+		my ($self, $controller) = @_;
+		# ...
+	}
+	# ...
+	sub some_func : TplExport {
+		my ($self, $controller, @your_args) = @_;
+		# ...
+		return '';
+	}
+	
+	<% some_func(@your_args) %>
 
 =head1 DESCRIPTION
 
 C<MojoX::Renderer::PSTemplate::ActionBase> is a Component Base class for
 MojoX::Renderer::PSTemplate meta framework on mojolicious. This class inherits
 all method from Text::PSTemplate::PluginBase.
+
+=head1 Template Functions
+
+=head2 param($name, [$escape])
+
+Returns GET parameter value.
+
+=head2 post_param($name, [$escape])
+
+Returns POST parameter value.
 
 =head1 METHODS
 
@@ -122,7 +154,7 @@ worry about it.
 =head2 patch
 =head2 trace
 =head2 link
-=head2 unlinke
+=head2 unlink
 
 These methods must be overridden by sub classes to act as a MVC Controller
 for to treats corresponding HTTP methods.
